1b8c55bf360cf55d66ac792fe5b7b6c60eed7d28
manuelmur <122873957+manuelmur@users.noreply.github.com>
2023-05-09 17:42:23 +0200

    Add files via upload

---
+++ Python/Raspberry/nicla_sc4.py
@@ -0,0 +1,31 @@
+import struct
+import asyncio
+import logging
+import time
+from bleak import BleakClient
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
+
+async def notif_handler(uuid, value: bytearray):
+	int_value = struct.unpack("<f", value)[0]
+	print("Dato: ", int(int_value))
+	print("Byte_array: ", value)
+	sender.send_message("/number", int(int_value))
+	
+
+async def run(address, service_uuid, char_uuid):
+	async with BleakClient(address) as client:
+		await client.start_notify(char_uuid, notif_handler)			
+		
+		while True:
+			await asyncio.sleep(1)
+			
+address = "90:01:71:96:EC:D3" # Dirección del dispositivo
+service_uuid = "19b10000-0000-537e-4f6c-d104768a1214" # UUID del servicio
+char_uuid = "19b10000-2001-537e-4f6c-d104768a1214" # UUID de la característica
+
+# Set up the OSC client
+sender = udp_client.SimpleUDPClient("127.0.0.1", 57120)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run(address, service_uuid, char_uuid))
