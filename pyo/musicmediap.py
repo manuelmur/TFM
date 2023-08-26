7d2cf2eb248763dd3c9cba2e7935b7fd62207b17
manuelmur <122873957+manuelmur@users.noreply.github.com>
2023-08-26 17:20:57 +0200

    Add files via upload

--- pyo/musicmediap.py
+++ pyo/musicmediap.py
@@ -4,42 +4,39 @@ import asyncio
 
 s = Server(audio="jack").boot() #,duplex=0).boot()
 
-# Create a Sig object for controlling the frequency
-freq = Sig(440)
-
-# Create a Port object for smooth frequency transitions
-#freq_port = Port(freq, risetime=0.01, falltime=0.01)
-
 def procesarestado(address, *args):
 	number = args[0]
 	#print(number)
 	if number==1:
-		#a.mul = 0
-		print("Muteado")
+		print("Distribucion weibull")
 		pit.setDist('weibull')
 	if number==0:
-		#a.mul = 1
-		print("Sonando")
-		pit.setDist('cauchy')
+		print("Distribucion poisson")
+		pit.setDist('poisson')
+		
+	#Eje Y
 	if number==2:
-		#freq.value = 220
 		wav.setOrder(1)
 	if number==3:
-		#freq.value = 440
 		wav.setOrder(2)
 	if number==4:
-		#freq.value = 680
 		wav.setOrder(4)
 	if number==5:
-		#freq.value = 900
 		wav.setOrder(6)
 	if number==6:
-		#freq.value = 1120
 		wav.setOrder(8)
-		
-
-#a = Sine(freq=freq, mul=1).out()
-#s.start()
+	
+	# Eje X
+	if number==7:
+		amp.setDur(2)
+	if number==8:
+		amp.setDur(1.5)
+	if number==9:
+		amp.setDur(1)
+	if number==10:
+		amp.setDur(.6)
+	if number==11:
+		amp.setDur(.25)
 
 s.start()
 wav = SquareTable()
@@ -52,4 +49,3 @@ out = Osc(table=wav, freq=pit, mul=amp).out()
 
 
 r = OscDataReceive(57120, "/estado", procesarestado)
-#r2 = OscDataReceive(57120, "/pitch", procesarpitch)
